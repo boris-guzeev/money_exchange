e9ba12242e58c85877d53bead0e21831831568cd
Boris Guzeev <guzeevboris@mail.ru>
2020-08-31 21:50:23 +0300

    add template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Обмен валют</title>
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
+</head>
+<body>
+<form id="exchangeForm" class="container">
+    <h2>Валютный калькулятор</h2>
+    <div class="row">
+        <div class="col-7">
+            <div class="row" style="margin-bottom: 15px;">
+                <label for="sum">Обменять</label>
+                <input type="text" name="sum" id="sum" class="form-control"/>
+            </div>
+            <div class="row">
+                <div class="col-5">
+                    <label>Из</label>
+                    <select name="rateFrom" id="rateFrom"></select>
+                </div>
+                <div class="col-2">
+                    <button type="submit" class="btn btn-primary" id="convertButton">Перевести</button>
+                </div>
+                <div class="col-5">
+                    <label for="rateTo">В</label>
+                    <select name="rateTo" id="rateTo"></select>
+                </div>
+                <input type="hidden" name="pair" id="pair"/>
+            </div>
+        </div>
+        <div class="col-5">
+            <label>Вы получите:</label>
+            <p>
+                <span class="result"></span> &#8381;
+            </p>
+            <p>
+                1 <span id="currencyLabel">$</span> = <span id="currancyRate"></span> &#8381;
+            </p>
+        </div>
+    </div>
+    <div class="row" id="messages"></div>
+</form>
+<script type="text/javascript" src="{{mix("/js/app.js")}}"></script>
+<script type="text/javascript" src="/js/script.js"></script>
+</body>
+</html>
