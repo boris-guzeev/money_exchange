6d6f6d2cbcd23f68140467ecbb2c705616d4a116
Boris Guzeev <guzeevboris@mail.ru>
2020-08-31 21:54:44 +0300

    контроллер конвертации

--- app/Http/Controllers/ConvertController.php
+++ app/Http/Controllers/ConvertController.php
@@ -1,3 +1,31 @@
+<?php
+/**
+ * Класс контроллер конвертации
+ */
+namespace App\Http\Controllers;
+
+use App\Models\Converter;
+use App\Models\Rate;
+use Illuminate\Http\Request;
+
+class ConvertController extends Controller {
+
+    /**
+     * Конвертирует валюту
+     * @param Request $request
+     * @return string
+     * @throws \ErrorException
+     */
+    function exchange(Request $request)
+    {
+        $pair = $request->input('pair');
+        $sum = $request->input('sum');
+        $currency = $request->input('rateTo');
+        $rate = Rate::where('pair', '=', $pair)->first()->rate;
+
+        return number_format(Converter::convert($pair, $rate, $sum, $currency), 2);
+    }
+
     /**
      * Возвращает пары валют
      * @return Rate[]|\Illuminate\Database\Eloquent\Collection
@@ -7,3 +35,5 @@
         $rates = Rate::all('pair');
         return $rates;
     }
+}
+
