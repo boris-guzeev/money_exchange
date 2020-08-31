26a2165288f3eb5f1099e2a5bb4b959ffa05a016
Boris Guzeev <guzeevboris@mail.ru>
2020-08-31 21:48:50 +0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MainController@index');
+Route::get('/exchange', 'ConvertController@exchange');
+Route::get('/get-pairs', 'ConvertController@getpairs');
